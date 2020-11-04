725e345612a4a6877b5375a763038784398bf7da
YuseiNaito <yusei.vitz.787@gmail.com>
2020-11-04 13:29:02 +0900

    リファクタリング

--- write.sh
+++ write.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
-mkdir /mnt/usb1
-mount /dev/sda1 /mnt/usb1
-ls /mnt/usb1
+mount_point=/mnt/usb1
+device=/dev/sdb1
+
+mkdir $mount_point
+mount $device $mount_point
+
+ls $mount_point
 
 if test -f sensor_a.csv; then
-  mv sensor_a.csv /mnt/usb1/sensor_a.csv
+  cp ./sensor_a.csv $mount_point/
+  rm sensor_a.csv
 fi
 
-umount /mnt/usb1
-rm -rf /mnt/usb1
+umount $mount_point
+rm -rf $mount_point
 touch sensor_a.csv
