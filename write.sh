6371460938be676f13c7eeb88218ab03aec29015
YuseiNaito <yusei.vitz.787@gmail.com>
2020-11-04 12:56:08 +0900

    USBメモリにファイルをムーブするように

--- write.sh
+++ write.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+mkdir /mnt/usb1
+mount /dev/sda1 /mnt/usb1
+ls /mnt/usb1
+
 if test -f sensor_a.csv; then
-  mv sensor_a.csv test/sensor_a.csv
+  mv sensor_a.csv /mnt/usb1/sensor_a.csv
 fi
 
+umount /mnt/usb1
+rm -rf /mnt/usb1
 touch sensor_a.csv
