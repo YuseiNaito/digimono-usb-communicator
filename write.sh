3ef223fcb4ffea18a46d30ffe5397983b60de480
YuseiNaito <yusei.vitz.787@gmail.com>
2020-11-25 10:47:34 +0900

    複数ファイルに対応

--- write.sh
+++ write.sh
@@ -4,8 +4,6 @@
 mount_point=/mnt/usb1
 # デバイス(USBメモリなど)のパス
 device=/dev/sda1
-# 取り出したいデータのパス
-file=sensor_a.csv
 # 書き込みが完了したか
 finished=false
 
@@ -13,12 +11,15 @@ while :
 do
   python ../BME280/Python27/bme280_sample.py
 
+  sleep 1
+
   if [ ! -e $device ]; then
     # デバイスが抜かれたらフラグリセット
     finished=false
+    continue
   fi
 
-  if [ -e $device -a $finished == false ]; then
+  if [ "$finished" = false ]; then
     # デバイスが存在し、まだ書き込まれていなければマウント
     if [ ! -d $mount_point ]; then
       # マウントポイントが存在しなければ作成
@@ -27,26 +28,27 @@ do
     mount $device $mount_point
     ls $mount_point
 
-    if [ ! -f $mount_point/$file ]; then
-      # デバイスに同名ファイルが存在しなければ
-      if [ -f $file ]; then
-        # データが存在すればばデバイスに移動
-        cp $file $mount_point/
-        rm $file
-        finished=true
-        echo 'Completed!'
-      fi
-      # 新規ファイル作成
-      touch $file
-    else
-      # デバイスに同名ファイルが存在したら書き込まない
-      echo 'Please remove old files.'
-    fi
+    for file in $(ls *.csv);
+    do
+      if [ ! -f $mount_point/$file ]; then
+        # デバイスに同名ファイルが存在しなければ
+        if [ -f $file ]; then
+          # データが存在すればばデバイスに移動
+          cp $file $mount_point/
+	  rm $file
+          finished=true
+          echo 'Completed!'
+        fi
+        # 新規ファイル作成
+        touch $file
+       else
+         # デバイスに同名ファイルが存在したら書き込まない
+         echo 'Please remove old files.'
+       fi
+    done
 
     # アンマウントし、マウントポイントを削除
     umount $mount_point
     rm -rf $mount_point
   fi
-
-  sleep 1
 done
